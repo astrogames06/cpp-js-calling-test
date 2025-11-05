f3d299dbf2affcafaeb6e317d16aedcf04035ed6
Jesse Sher <jebba272727@gmail.com>
2025-11-06 08:14:19 +1100

    changed a element in the DOM using c++

--- main.cpp
+++ main.cpp
@@ -28,16 +28,36 @@ void SetLocal(const std::string& key, int value)
 int add(int a, int b) {
     return a + b;
 }
+void set_text()
+{
+    emscripten::val document = emscripten::val::global("document");
+
+    emscripten::val p = document.call<emscripten::val>("getElementById", std::string("text_to_change"));
+    p.set("innerText", std::string("THIS TEXT HAS BEEN SET BY C++!"));
+}
+
 EMSCRIPTEN_BINDINGS(function_module) {
     emscripten::function("add", &add);
+    emscripten::function("SetText", &set_text);
 }
 
 int main() {
+    // Alerting
     Alert("Hello, from C++! :)");
 
+    // Saving local storage
     std::string ITEM = std::to_string(GetLocal("num"));
     Alert(ITEM);
     SetLocal("num", GetLocal("num") + 1);
     std::string NEW_ITEM = "NEW_ITEM: " + std::to_string(GetLocal("num"));
     Alert(NEW_ITEM);
+
+    // Adding items to DOM
+    emscripten::val document = emscripten::val::global("document");
+    emscripten::val body = document["body"];
+
+    emscripten::val p = document.call<emscripten::val>("createElement", std::string("p"));
+    p.set("innerText", std::string("Hello from C++!"));
+
+    body.call<void>("appendChild", p);
 }
