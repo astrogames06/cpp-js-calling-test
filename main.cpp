f6575158e6d3071f912922fdfaeecf1d51dd4efb
Jesse Sher <jebba272727@gmail.com>
2025-11-06 13:18:35 +1100

    Added button that changes color

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <emscripten/emscripten.h>
 #include <emscripten/val.h>
 #include <emscripten/bind.h>
@@ -35,10 +37,24 @@ void set_text()
     emscripten::val p = document.call<emscripten::val>("getElementById", std::string("text_to_change"));
     p.set("innerText", std::string("THIS TEXT HAS BEEN SET BY C++!"));
 }
+void set_red()
+{
+    emscripten::val document = emscripten::val::global("document");
+
+
+    // testing in one line!
+    // Gets element with id 'made_cpp'
+    // Then calls the classList function and adds the red_class class
+    document.call<emscripten::val>
+        ("getElementById", std::string("made_cpp"))
+        ["classList"].call<void>
+        ("add", std::string("red_class"));
+}
 
 EMSCRIPTEN_BINDINGS(function_module) {
     emscripten::function("add", &add);
     emscripten::function("SetText", &set_text);
+    emscripten::function("SetRed", &set_red);
 }
 
 int main() {
@@ -47,17 +63,18 @@ int main() {
 
     // Saving local storage
     std::string ITEM = std::to_string(GetLocal("num"));
-    Alert(ITEM);
+    std::cout << ITEM << '\n';
     SetLocal("num", GetLocal("num") + 1);
     std::string NEW_ITEM = "NEW_ITEM: " + std::to_string(GetLocal("num"));
-    Alert(NEW_ITEM);
+    std::cout << NEW_ITEM << '\n';
 
     // Adding items to DOM
     emscripten::val document = emscripten::val::global("document");
     emscripten::val body = document["body"];
 
     emscripten::val p = document.call<emscripten::val>("createElement", std::string("p"));
-    p.set("innerText", std::string("Hello from C++!"));
+    p.set("id", std::string("made_cpp"));
+    p.set("innerText", std::string("Hello from C++! (By in C++)"));
 
     body.call<void>("appendChild", p);
 }
